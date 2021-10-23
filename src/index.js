6d2bbad3dfe54f1258c57eed9dd09da7f9b7f191
Nick Hansel <holywhale7@gmail.com>
2021-10-22 19:44:54 -0500

    fnc

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+export TWITTER_BEARER_TOKEN=<////////>
+
 ReactDOM.render(
   < App />,
   document.getElementById('root')
