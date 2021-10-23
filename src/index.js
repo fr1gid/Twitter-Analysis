29ada7ab091ad20c4cb38f8922b3d418a3fe496b
Nick Hansel <holywhale7@gmail.com>
2021-10-22 19:50:21 -0500

    ebf

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-export TWITTER_BEARER_TOKEN=<////////>
+import { TwitterClient } from 'twitter-api-client';
+
+const twitterClient = new TwitterClient({
+  apiKey: '<YOUR-TWITTER-API-KEY>',
+  apiSecret: '<YOUR-TWITTER-API-SECRET>',
+  accessToken: '<YOUR-TWITTER-ACCESS-TOKEN>',
+  accessTokenSecret: '<YOUR-TWITTER-ACCESS-TOKEN-SECRET>',
+});
+
+// Search for a user
+const data = await twitterClient.accountsAndUsers.usersSearch({ q: 'twitterDev' });
+
+// Get message event by Id
+const data = await twitterClient.directMessages.directMessagesEventsShow({ id: '1234' });
+
+// Get most recent 25 retweets of a tweet
+const data = await twitterClient.tweets.statusesRetweetsById({ id: '12345', count: 25 });
+
+// Get local trends
+const data = await twitterClient.trends.trendsAvailable();
 
 ReactDOM.render(
   < App />,
