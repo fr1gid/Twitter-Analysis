1ba5e903840179bacc617222d577fcc6d6d7d761
Nick Hansel <holywhale7@gmail.com>
2021-10-22 20:09:00 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+export default class FormSubmission extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchQuery: ""
+    }
+  }
+
+  handleInputChanged(event) {
+    this.setState({
+      searchQuery: event.target.value
+    });
+  }
+
+  handleButtonClicked() {
+    var searchQuery = this.state.searchQuery;
+  }
+
+  render() {
+    return  (
+      <div>
+        <input type="text" value={this.state.searchQuery} onChange={this.handleInputChanged.bind(this)}/>
+        <button onClick={this.handleButtonClicked.bind(this)}>
+          Submit
+        </button>
+      </div>
+    );
+  }
 }
-
-export default App
 
 
--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { TwitterClient } from 'twitter-api-client';
-
-const twitterClient = new TwitterClient({
-  apiKey: '<1OpsPpaPvqWtg0M4Q3ZDa44fk>',
-  apiSecret: '<6wXI9TJLeGMOMtXx4pnLoNm5UfsqsArXSWkH9OGxSTFY9GoLrG>',
-  accessToken: '<1335744879369052160-pDBFJabJ9a10OUsyDaTJ68ccxfIhFB>',
-  accessTokenSecret: '<vuxTI7VspWHDVWj5OfUoZd9RlhQtrg3Vk13rVpcYJUVO4>',
-});
-
-// Search for a user
-const data = await twitterClient.accountsAndUsers.usersSearch({ q: 'twitterDev' });
-
-// Get message event by Id
-const data = await twitterClient.directMessages.directMessagesEventsShow({ id: '1234' });
-
-// Get most recent 25 retweets of a tweet
-const data = await twitterClient.tweets.statusesRetweetsById({ id: '12345', count: 25 });
-
-// Get local trends
-const data = await twitterClient.trends.trendsAvailable();
-
 ReactDOM.render(
   < App />,
   document.getElementById('root')
